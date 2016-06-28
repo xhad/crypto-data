20206bd3685a3d9cd32c432056ac90710efd829e
xhad <chyad@hotmail.com>
2016-06-28 23:41:21 +0900

    updates

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
 'use strict';
 var clc = require('cli-color');
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/bfx');
+var db = mongoose.connect('mongodb://localhost:27017/bitfinex_usd');
 var Schema = mongoose.Schema;
 var TradesSchema = new Schema({
-   seq: String,
-   timestamp: Number,
-   price: Number,
-   amount: Number
+    tid: String,
+    timestamp: Number,
+    price: Number,
+    amount: Number
 });
 var Trades = mongoose.model('socketTrades', TradesSchema);
 var ws = function(api_key, api_secret) {
-var WebSocket = require('ws');
-var ws = new WebSocket("wss://api2.bitfinex.com:3000/ws");
-   ws.api_key = 'KEY';
-   ws.api_secret = 'SECRET';
-   ws.on('open', function open() {
-      ws.send(JSON.stringify({
-         "event": "subscribe",
-         "channel": "trades",
-         "pair": "BTCUSD"
-      }));
-   });
-   ws.on('message', function(data, flags) {
+    var WebSocket = require('ws');
+    var ws = new WebSocket("wss://api2.bitfinex.com:3000/ws");
+    ws.api_key = 'KEY';
+    ws.api_secret = 'SECRET';
+    ws.on('open', function open() {
+        ws.send(JSON.stringify({
+            "event": "subscribe",
+            "channel": "trades",
+            "pair": "BTCUSD"
+        }));
+    });
+    ws.on('message', function(data, flags) {
         var data = JSON.parse(data);
-       var p = data[3];
-      var a = data[4];
-      if (a > 0) {
-         p = clc.green(p)
-      } else {
-         p = clc.red(p)
-      };
-      console.log(clc.blue('ID: ' + clc.yellow(data[1])) +
-         clc.blue(' Time: ' + clc.yellow(data[2])) +
-         clc.blue(' Price: ') + p +
-         clc.blue(' Amount: ' + clc.white(data[4]))
+        console.log(data);
+        var tid = data[2];
+        var timestamp = data[3];
+        var price = data[4];
+        var amount = data[5];
+        var latest = new Trades({
+            tid: data[1],
+            timestamp: +timestamp,
+            price: +price,
+            amount: +amount
+        });
 
-      );
-      var latest = new Trades({
-         seq: data[1],
-         timestamp: data[2],
-         price: data[3],
-         amount: data[4]
-      });
-      latest.save(function(err) {
-         if (err) throw err;
-         console.log("saved");
-      });
-   })
+        if (timestamp)
+          latest.save(function(err) {
+            if (err) throw err;
+            console.log("saved");
+        });
+    })
 };
 ws();
